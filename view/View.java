78ab2395eb4c5b64c34ee2bfb1bf3bb7e3a61016
Codelobster <codelobster@yandex.ru>
2023-10-09 22:13:27 +0300

    Lesson 5 - SaveAs

--- view/View.java
+++ view/View.java
@@ -5,7 +5,7 @@ import model.Contact;
 import java.util.List;
 
 public interface View {
-    public void printAll(List<Contact> phoneBook);
+    void printAll(List<Contact> phoneBook);
 
-    public void successFul(Boolean result, String operation, String comment);
+    void successFul(Boolean result, String operation, String comment);
 }
